558d4d1f0f3f574ca870fe7e21ec1042651257f1
irahulcse <1rahulchandra1@gmail.com>
2020-02-12 03:58:29 +0530

    Changed the name

---
+++ calculator_app_with_animation/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+void main()=> runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
