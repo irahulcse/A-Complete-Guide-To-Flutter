8d7cbad195eefdc2c89186189d3e74e769f78686
irahulcse <1rahulchandra1@gmail.com>
2020-03-29 12:08:56 +0530

    Flutter Quiz Application using JSON

--- 020_quiz_application_flutter/lib/main.dart
+++ 020_quiz_application_flutter/lib/main.dart
@@ -1,13 +1,160 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-void main()=> runApp(new MyApp());
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:quiz_application_flutter/rahul.dart';
+
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return MaterialApp(
+      home: new HomePage(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
     );
   }
 }
 
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Rahul rahul;
+  List<Results> results;
+
+  Future<void> fetchQuestions() async {
+    var res = await http.get('https://opentdb.com/api.php?amount=20');
+    var decRes = jsonDecode(res.body);
+    //print(decRes);
+    rahul = Rahul.fromJson(decRes);
+    results = rahul.results;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        title: Text('Quiz App'),
+        elevation: 0.0,
+      ),
+      body: new FutureBuilder(
+        future: fetchQuestions(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              return Text("Press Hold To Start:");
+            case ConnectionState.active:
+            case ConnectionState.waiting:
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            case ConnectionState.done:
+              if (snapshot.hasError) return Container();
+              return questionList();
+          }
+          return null;
+        },
+      ),
+    );
+  }
+
+  ListView questionList() {
+    return ListView.builder(
+      itemCount: results.length,
+      itemBuilder: (context, index) => Card(
+        color: Colors.white,
+        elevation: 0.0,
+        child: ExpansionTile(
+          title: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                results[index].question,
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              FittedBox(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    FilterChip(
+                      backgroundColor: Colors.grey[100],
+                      label: Text(
+                        results[index].category,
+                      ),
+                      onSelected: (b) {},
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    FilterChip(
+                      backgroundColor: Colors.grey[100],
+                      label: Text(
+                        results[index].difficulty,
+                      ),
+                      onSelected: (b) {},
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          leading: CircleAvatar(
+            backgroundColor: Colors.grey[100],
+            child: Text(
+              results[index].type.startsWith("m") ? "M" : "B",
+            ),
+          ),
+          children: results[index].allAnswers.map((m) {
+            return AnswerWidget(results, index, m);
+          }).toList(),
+        ),
+      ),
+    );
+  }
+}
+
+class AnswerWidget extends StatefulWidget {
+  final List<Results> results;
+  final int index;
+  final String m;
+
+  AnswerWidget(this.results, this.index, this.m);
+  @override
+  _AnswerWidgetState createState() => _AnswerWidgetState();
+}
+
+class _AnswerWidgetState extends State<AnswerWidget> {
+  Color c = Colors.black;
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: () {
+        setState(() {
+          if (widget.m == widget.results[widget.index].correctAnswer) {
+            c = Colors.green;
+          } else {
+            c = Colors.redAccent;
+          }
+        });
+      },
+      title: Text(
+        widget.m,
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: c,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+}
