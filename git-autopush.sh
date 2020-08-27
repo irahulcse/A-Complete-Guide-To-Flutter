cbea6ffc310e43a8148f9dde197016446c69657a
irahulcse <1rahulchandra1@gmail.com>
2020-08-27 21:38:13 +0530

    Flutter Virtual Commit using Bash 1.0.1(First Commit) 2020-08-27-21:38:13

--- git-autopush.sh
+++ git-autopush.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 git pull origin master
 git add .
-git commit -m "First Commit at `date +%F-%T`"
-git push origin master
+git commit -m "Flutter Virtual Commit using Bash 1.0.1(First Commit) `date +%F-%T`"
+git push -f origin master
